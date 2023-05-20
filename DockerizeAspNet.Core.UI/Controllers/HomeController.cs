e8f003ff0ce5ef1f7c91ce114d3d919c85871aec
Erkan Bostan <profesyonel59@gmail.com>
2023-05-20 05:56:06 +0300

    Add project files.

---
+++ DockerizeAspNet.Core.UI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.Diagnostics;
+
+namespace DockerizeAspNet.Core.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFileProvider _fileProvider;
+        public HomeController(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().Select(x => x.Name);
+            return View(images);
+        }
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().Select(x => x.Name == name);
+            System.IO.File.Delete("wwwroot/Images/" + name);
+            return RedirectToAction("ImageShow");
+        }
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile formFile)
+        {
+            if (formFile != null && formFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+            return View();
+        }
+    }
+}
